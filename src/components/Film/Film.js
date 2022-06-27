4b98c3316e2159e42ccb996d601ccfe80ed9b52b
Bartek Swiderski <b.swiderski92@gmail.com>
2022-06-27 10:50:20 +0100

    add styling and bg img

--- src/components/Film/Film.js
+++ src/components/Film/Film.js
@@ -5,11 +5,14 @@ import "./Film.css"
 export const Film = ({ image, title, description }) => {
     return (
         <div className="film">
-            <img src={image} alt={`${title} cover}`} />
+            <img className="film-img" src={image} alt={`${title} cover`} />
             <div class="film-info">
                 <h2 class="film-info__title">{title}</h2>
                 <p class="film-info__description">{description}</p>
                 <Button className="film-info__button" buttonContent="See More" />
+                <div class="film-info__background-image">
+                    <img src={require("../../assets/totoro.png")} alt="" />
+                </div>
             </div>
         </div>
     )
