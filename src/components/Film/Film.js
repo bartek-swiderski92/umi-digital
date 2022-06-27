0527a2b3364455ab662a71ccb8a7f29964b3e5e5
Bartek Swiderski <b.swiderski92@gmail.com>
2022-06-27 11:02:04 +0100

    update classes

--- src/components/Film/Film.js
+++ src/components/Film/Film.js
@@ -6,11 +6,11 @@ export const Film = ({ image, title, description }) => {
     return (
         <div className="film">
             <img className="film-img" src={image} alt={`${title} cover`} />
-            <div class="film-info">
-                <h2 class="film-info__title">{title}</h2>
-                <p class="film-info__description">{description}</p>
+            <div className="film-info">
+                <h2 className="film-info__title">{title}</h2>
+                <p className="film-info__description">{description}</p>
                 <Button className="film-info__button" buttonContent="See More" />
-                <div class="film-info__background-image">
+                <div className="film-info__background-image">
                     <img src={require("../../assets/totoro.png")} alt="" />
                 </div>
             </div>
