d04ccbb45bc6f537e1212103f479b53252fc3533
Bartek Swiderski <b.swiderski92@gmail.com>
2022-06-27 12:46:11 +0100

    add background image

--- src/components/Film/Film.js
+++ src/components/Film/Film.js
@@ -9,7 +9,11 @@ export const Film = ({ filmId, image, title, description }) => {
 
     return (
         <div className="film">
-            <img className="film-img" src={image} alt={`${title} cover`} />
+            <div class="film-img-container">
+                <img className="film-img" src={image} alt={`${title} cover`} />
+                <div className="background-img"/>
+
+            </div>
             <div className="film-info">
                 <h2 className="film-info__title">{title}</h2>
                 <p className="film-info__description">{description}</p>
